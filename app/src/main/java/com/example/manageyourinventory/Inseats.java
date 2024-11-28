27baae3522659f4a4bfcb92f56c36f6d279395cc
Ankita Kumari <ankita.ac.dv@gmail.com>
2024-11-28 14:08:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/manageyourinventory/Inseats.java
@@ -0,0 +1,4 @@
+package com.example.manageyourinventory;
+
+public class Inseats {
+}
--- app/src/main/java/com/example/manageyourinventory/supply.java
+++ app/src/main/java/com/example/manageyourinventory/supply.java
@@ -1,35 +1,24 @@
 package com.example.manageyourinventory;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.ViewModelProvider;
 
-public class Dashboard extends AppCompatActivity {
+public class supply extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_supply);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void product(View view) {
-        startActivity(new Intent(this, product.class));
-    }
-
-    public void supply(View view) {
-        startActivity(new Intent(this, supply.class));
     }
 }
