3888ea22fd4f474e0cca70a8959190a989a0b4ab
Ankita Kumari <ankita.ac.dv@gmail.com>
2024-11-29 11:54:37 +0530

    Initial Commit

--- app/src/main/java/com/example/manageyourinventory/cost.java
+++ app/src/main/java/com/example/manageyourinventory/cost.java
@@ -1,6 +1,5 @@
 package com.example.manageyourinventory;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,15 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.ViewModelProvider;
 
-public class Dashboard extends AppCompatActivity {
+public class cost extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_cost);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -25,19 +23,4 @@ public class Dashboard extends AppCompatActivity {
         });
     }
 
-    public void product(View view) {
-        startActivity(new Intent(this, product.class));
-    }
-
-    public void supply(View view) {
-        startActivity(new Intent(this, supply.class));
-    }
-
-    public void cost(View view) {
-        startActivity(new Intent(this, cost.class));
-    }
-
-    public void center(View view) {
-        startActivity(new Intent(this, center.class));
-    }
 }
